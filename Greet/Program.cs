a16ab3a3b40e5ec8ee7e80c72f284e1f0f1da571
Jon <j.crawford@gmail.com>
2020-04-10 17:10:05 -0400

    initial commit

---
+++ Greet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
